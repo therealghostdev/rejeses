1bcf77c662404f72d2ea705b793ebd76d75b6ecb
DaScOrPiO <dunni82@yahoo.com>
2024-07-31 13:57:25 +0100

    FEAT: create transaction api route

--- src/app/api/transaction/route.ts
+++ src/app/api/transaction/route.ts
@@ -5,6 +5,7 @@ import {
   updateTransaction,
 } from "@/app/services/repository/transactions/transactions";
 import { StatusType, TransactionType } from "@/utils/types/types";
+import { getOrdeById } from "@/app/services/repository/order/order";
 
 export async function GET(req: Request) {
   try {
@@ -70,5 +71,76 @@ export async function GET(req: Request) {
   } catch (err) {
     console.log(err);
     Response.json({ message: "Error getting transaction", status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    let body;
+    try {
+      body = await req.json();
+    } catch (err) {
+      console.log(err);
+      return Response.json(
+        { message: "Invalid JSON in request body" },
+        { status: 400 }
+      );
+    }
+
+    const { txid, ref, pid, reference, status, accessCode, currency, _fee } =
+      body;
+
+    const orderRef = Number(ref);
+    const fee = Number(_fee);
+    const requiredFields = {
+      txid,
+      orderRef,
+      pid,
+      reference,
+      status,
+      accessCode,
+      currency,
+      fee,
+    };
+
+    if (
+      Object.values(requiredFields).some(
+        (fields) => fields === null || fields === "" || fields === undefined
+      )
+    )
+      return Response.json(
+        { message: "Missing or empty body parameters" },
+        { status: 400 }
+      );
+
+    if (isNaN(orderRef) || isNaN(fee))
+      return Response.json(
+        {
+          message: "Invalid fields! 'ref' or '_fee' fields must be a number",
+        },
+        { status: 400 }
+      );
+
+    if (!Object.values(StatusType).includes(status as StatusType))
+      return Response.json(
+        { message: "Invalid status value" },
+        { status: 400 }
+      );
+
+    const order = await getOrdeById(orderRef);
+
+    if (!order)
+      return Response.json(
+        { message: "Order for this transaction not found" },
+        { status: 404 }
+      );
+
+    // return Response.json({ data: order, message: "sucessful", status: 200 });
+
+    await createTransaction(requiredFields);
+    return Response.json({ message: "Success" }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    Response.json({ message: "Error creating transaction" }, { status: 500 });
   }
 }
