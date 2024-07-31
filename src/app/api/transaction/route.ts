60c8ae15e63906fd95d340483b8b951c5b10920c
DaScOrPiO <dunni82@yahoo.com>
2024-07-31 14:54:55 +0100

    FEAT: update transaction api route

--- src/app/api/transaction/route.ts
+++ src/app/api/transaction/route.ts
@@ -6,6 +6,7 @@ import {
 } from "@/app/services/repository/transactions/transactions";
 import { StatusType, TransactionType } from "@/utils/types/types";
 import { getOrdeById } from "@/app/services/repository/order/order";
+import { Transaction } from "@prisma/client";
 
 export async function GET(req: Request) {
   try {
@@ -135,12 +136,88 @@ export async function POST(req: Request) {
         { status: 404 }
       );
 
-    // return Response.json({ data: order, message: "sucessful", status: 200 });
-
     await createTransaction(requiredFields);
     return Response.json({ message: "Success" }, { status: 200 });
   } catch (err) {
     console.log(err);
     Response.json({ message: "Error creating transaction" }, { status: 500 });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+
+    if (!id) return Response.json({ message: "Transaction id is required" });
+
+    let body;
+    try {
+      body = await req.json();
+    } catch (err) {
+      console.log(err);
+      return Response.json(
+        { mesage: "Invalid JSON in request body" },
+        { status: 400 }
+      );
+    }
+
+    const { txid, pid, reference, status, accessCode, currency, _fee } = body;
+
+    const fee = _fee && Number(_fee);
+
+    const validFields: Partial<TransactionType> = {};
+
+    if (txid !== undefined) validFields.txid = txid;
+    if (pid !== undefined) validFields.pid = pid;
+    if (reference !== undefined) validFields.reference = reference;
+    if (status !== undefined) validFields.status = status as StatusType;
+    if (accessCode !== undefined) validFields.accessCode = accessCode;
+    if (currency !== undefined) validFields.currency = currency;
+    if (fee !== undefined) validFields.fee = fee;
+
+    const containsEmptyString = Object.values(validFields).some(
+      (fields) => fields === ""
+    );
+
+    if (Object.keys(validFields).length === 0)
+      return Response.json(
+        { message: "No valid field were provided" },
+        { status: 400 }
+      );
+
+    if (containsEmptyString)
+      return Response.json({ message: "Invalid field value" }, { status: 400 });
+
+    if (_fee && isNaN(fee))
+      return Response.json(
+        {
+          message: "Invalid field! '_fee' field must be a number",
+        },
+        { status: 400 }
+      );
+
+    if (status && !Object.values(StatusType).includes(status as StatusType))
+      return Response.json(
+        { message: "Invalid status value" },
+        { status: 400 }
+      );
+
+    const transaction = await getTransactionById(Number(id));
+
+    if (!transaction)
+      return Response.json(
+        { message: "Transaction not found" },
+        { status: 404 }
+      );
+
+    await updateTransaction(Number(id), validFields);
+    return Response.json({ message: "Successful" }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return Response.json(
+      { message: "Error updating transaction" },
+      { status: 500 }
+    );
   }
 }
