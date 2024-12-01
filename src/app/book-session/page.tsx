b18cfcf859c17fa3dcba84c54bb0fda60663930f
Oladunni Faith <dunni82@yahoo.com>
2024-12-01 19:00:54 +0100

    FEAT: book a session page implementation

---
+++ src/app/book-session/page.tsx
@@ -0,0 +1,9 @@
+import BookSessionPage from "@/components/web_pages/book_a_session/book_a_session";
+
+export default function Page() {
+  return (
+    <section>
+      <BookSessionPage />
+    </section>
+  );
+}
