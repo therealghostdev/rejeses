a1bc5e1e8a6895fd3b400f59cb0d5594d3c3d60e
Sam <125397952+rejeses@users.noreply.github.com>
2024-12-09 07:04:45 +0100

    Update terms_of_use.tsx

--- src/components/general/footer/terms_of_use.tsx
+++ src/components/general/footer/terms_of_use.tsx
@@ -5,11 +5,11 @@ export default function TermsOfUse() {
     <div className="flex items-center justify-center w-full py-6 px-4 md:bg-inherit max-md:bg-gray-100 sm:bg-gray-100 text-[#5B5B5B]">
       <div className="flex space-x-4 justify-center items-center w-full">
         <Link href="/termsandconditions" className="text-lg transition_border">
-          Terms of Use
+          TERMS OF USE
         </Link>
         <span className="border-r-2 border-[#535353] h-6 mx-4"></span>
         <Link href="/privacy-policy" className="text-lg transition_border">
-          Privacy Policy
+          PRIVACY POLICY
         </Link>
       </div>
     </div>
