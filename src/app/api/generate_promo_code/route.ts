ea9e4dbfc52e63058484efdeb52f1c08bd301c03
Oladunni Faith <dunni82@yahoo.com>
2025-02-02 23:05:02 +0100

    Update route.ts

--- src/app/api/generate_promo_code/route.ts
+++ src/app/api/generate_promo_code/route.ts
@@ -1,24 +1,24 @@
-import { generatePromoCode } from "@/app/services/repository/promocode/promocode";
-import fs from "fs/promises";
-import path from "path";
+// import { generatePromoCode } from "@/app/services/repository/promocode/promocode";
+// import fs from "fs/promises";
+// import path from "path";
 
-const CONFIG_PATH = path.join(process.cwd(), "public/promo/promo.json");
+// const CONFIG_PATH = path.join(process.cwd(), "public/promo/promo.json");
 
-export async function GET() {
-  try {
-    const configData = await fs.readFile(CONFIG_PATH, "utf-8");
-    const config = JSON.parse(configData);
+// export async function GET() {
+//   try {
+//     const configData = await fs.readFile(CONFIG_PATH, "utf-8");
+//     const config = JSON.parse(configData);
 
-    const today = new Date();
-    const nigeriaDate = new Date(today.getTime() + 1 * 60 * 60 * 1000);
-    const nigeriaDay = nigeriaDate.getDay();
+//     const today = new Date();
+//     const nigeriaDate = new Date(today.getTime() + 1 * 60 * 60 * 1000);
+//     const nigeriaDay = nigeriaDate.getDay();
 
-    if (config.isPromo && nigeriaDay === 1) {
-      await generatePromoCode();
-      return Response.json({ success: true, message: "Promo code generated" });
-    }
-    return Response.json({ success: false, message: "Promo is disabled" });
-  } catch (err) {
-    console.log("Error generating promo code:", err);
-  }
-}
+//     if (config.isPromo && nigeriaDay === 1) {
+//       await generatePromoCode();
+//       return Response.json({ success: true, message: "Promo code generated" });
+//     }
+//     return Response.json({ success: false, message: "Promo is disabled" });
+//   } catch (err) {
+//     console.log("Error generating promo code:", err);
+//   }
+// }
